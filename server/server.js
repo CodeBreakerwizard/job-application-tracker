a1ec2a1611c1d1f4373ff347c19d4ff09c93c9d5
CodeBreakerwizard <chauhansarthak2004@gmail.com>
2026-08-29 16:19:33 +0530

    Setup MongoDB database connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-const PORT = 5000;
-
 app.get("/", (req, res) => {
-    res.send("Job Application Tracker API is running!");
+    res.json({
+        message: "Job Application Tracker API is running!",
+        status: "success",
+    });
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
